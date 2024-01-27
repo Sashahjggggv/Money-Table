6bff0cdccf57d62236420251fb934ef2c818379a
ALEX <sg014877@gmail.com>
2024-01-27 12:12:13 +0200

    new exchange rates

--- js/rates.js
+++ js/rates.js
@@ -1,25 +1,24 @@
 // Вписати курси валют в банках на даний момент (Картковий і Готівковий)
-let privatCard = 38.1659; let privatCash = 38.15;
+let privatCard = 38.1679; let privatCash = 38.2;
 let monoCard = 38.0199; let monoCash = 0;
-let abankCard = 38.023; let abankCash = 38.5;
-let oschadCard = 38.1; let oschadCash = 38.2;
-let raifCard = 38; let raifCash = 38.05;
-let otpCard = 38.05; let otpCash = 38.25;
+let abankCard = 38.168; let abankCash = 38.5;
+let oschadCard = 38.2; let oschadCash = 38.3;
+let raifCard = 38.15; let raifCash = 38.2;
+let otpCard = 38.05; let otpCash = 38.35;
 let iziCard = 38; let iziCash = 37.95;
-let taskomCard = 0; let taskomCash = 38.35;
+let taskomCard = 0; let taskomCash = 38.45;
 let pumbCard = 38.1; let pumbCash = 38.3;
 let globusCard = 37.9; let globusCash = 38.3;
-let ukrgasCard = 38.1; let ukrgasCash = 38.2;
+let ukrgasCard = 38.2; let ukrgasCash = 38.3;
 let praveksCard = 38.05; let praveksCash = 38.2;
-let ideaCard = 38.1; let ideaCash = 38.25;
+let ideaCard = 38.1; let ideaCash = 38.35;
 let ukreximCard = 37.9; let ukreximCash = 38.25;
 let kredoCard = 38.36; let kredoCash = 38.39;
-let ukrsibCard = 38.05; let ukrsibCash = 38.2;
+let ukrsibCard = 38.15; let ukrsibCash = 38.3;
 let investzaoschCard = 38.2; let investzaoschCash = 38.25;
 let krediagrikoleCard = 38.2; let krediagrikoleCash = 38.3;
-let senseCard = 38.1; let senseCash = 38.2;
-let prokreditCard = 38; let prokreditCash = 38.2;
-let poltavaCard = 38; let poltavaCash = 38.29;
+let senseCard = 38.15; let senseCash = 38.3;
+let poltavaCard = 38; let poltavaCash = 38.45;
 
 // немає в Рівному
 let kominCard = 0; let kominCash = 0;
@@ -34,3 +33,4 @@ let kominvestCard = 0; let kominvestCash = 0;
 let piraeusCard = 0; let piraeusCash = 0;
 let skyCard = 0; let skyCash = 0;
 let threeforCard = 0; let threeforCash = 0;
+let prokreditCard = 0; let prokreditCash = 0;
