79a7b428e55f6c05094c51aeeccef1193819eb6e
ALEX <sg014877@gmail.com>
2024-01-31 13:48:09 +0200

    new exchange rates

--- js/rates.js
+++ js/rates.js
@@ -1,24 +1,24 @@
 // Вписати курси валют в банках на даний момент (Картковий і Готівковий)
-let privatCard = 38.1679; let privatCash = 38.2;
-let monoCard = 38.0199; let monoCash = 0;
-let abankCard = 38.168; let abankCash = 38.5;
-let oschadCard = 38.2; let oschadCash = 38.3;
-let raifCard = 38.15; let raifCash = 38.2;
-let otpCard = 38.05; let otpCash = 38.35;
-let iziCard = 38; let iziCash = 37.95;
-let taskomCard = 0; let taskomCash = 38.45;
-let pumbCard = 38.1; let pumbCash = 38.3;
-let globusCard = 37.9; let globusCash = 38.3;
-let ukrgasCard = 38.2; let ukrgasCash = 38.3;
-let praveksCard = 38.05; let praveksCash = 38.2;
-let ideaCard = 38.1; let ideaCash = 38.35;
+let privatCard = 38.1679; let privatCash = 38.1;
+let monoCard = 38.1607; let monoCash = 0;
+let abankCard = 38.168; let abankCash = 38.3;
+let oschadCard = 38.1; let oschadCash = 38.1;
+let raifCard = 37.85; let raifCash = 38;
+let otpCard = 38; let otpCash = 38.25;
+let iziCard = 38; let iziCash = 38.05;
+let taskomCard = 0; let taskomCash = 38.05;
+let pumbCard = 38.1; let pumbCash = 38.2;
+let globusCard = 38; let globusCash = 38.05;
+let ukrgasCard = 38.1; let ukrgasCash = 38.2;
+let praveksCard = 37.95; let praveksCash = 38.2;
+let ideaCard = 38.2; let ideaCash = 38.35;
 let ukreximCard = 37.9; let ukreximCash = 38.25;
-let kredoCard = 38.36; let kredoCash = 38.39;
-let ukrsibCard = 38.15; let ukrsibCash = 38.3;
-let investzaoschCard = 38.2; let investzaoschCash = 38.25;
-let krediagrikoleCard = 38.2; let krediagrikoleCash = 38.3;
-let senseCard = 38.15; let senseCash = 38.3;
-let poltavaCard = 38; let poltavaCash = 38.45;
+let kredoCard = 38.56; let kredoCash = 38.09;
+let ukrsibCard = 38.2; let ukrsibCash = 38.2;
+let investzaoschCard = 38.3; let investzaoschCash = 38.1;
+let krediagrikoleCard = 38.1; let krediagrikoleCash = 38.14;
+let senseCard = 38.05; let senseCash = 38.1;
+let poltavaCard = 38; let poltavaCash = 0;
 
 // немає в Рівному
 let kominCard = 0; let kominCash = 0;
